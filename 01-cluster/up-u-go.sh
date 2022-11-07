a73e9ab856616e8a953a630e59f7aa37afdcc711
Audryus <audryus@hotmail.com>
2022-11-07 20:18:48 -0300

    adding descriptive shell

--- 01-cluster/up-u-go.sh
+++ 01-cluster/up-u-go.sh
@@ -1,8 +1,12 @@
+echo 'Creating cluster ...'
 k3d cluster create one-cluster -p "80:80@loadbalancer" -p "443:443@loadbalancer" --wait
 #openssl genrsa -out ca.key 4096
 #openssl req -new -x509 -sha256 -days 10950 -key ca.key -out ca.crt
+echo 'Cert manager ...'
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.0/cert-manager.yaml
+echo 'Waiting Cert manager finish install ...'
 kubectl wait deployment cert-manager-webhook -n cert-manager --for condition=Available=True --timeout=-1s
+echo 'Waiting Cluster finish install ...'
 kubectl wait -n kube-system job.batch/helm-install-traefik-crd --for=condition=complete --timeout=-1s
 export TLS_CRT="$(cat ca.crt | base64 -w 0)"
 export TLS_KEY="$(cat ca.key | base64 -w 0)"
@@ -11,5 +15,7 @@ kubectl create namespace traefik
 kubectl apply -n traefik -f traefik-cert.yaml
 kubectl apply -n traefik -f traefik-mdw-redirecttohttps.yaml
 kubectl apply -n traefik -f traefik-dashboard.yaml
+echo 'Waiting install ...'
 sleep 10
 kubectl apply -n cert-manager -f cluster-issuer.yaml
+echo 'Finish ;-)'
--- 02-argocd/dev-argo.sh
+++ 02-argocd/dev-argo.sh
@@ -1,3 +1,8 @@
+echo 'Installing Argo CD ...'
 kubectl apply -k overlays/dev
+echo 'Waiting Argo CD ...'
 kubectl wait deployment argocd-server -n argocd --for condition=Available=True --timeout=-1s
+echo 'Applying GitOps Argo CD ...'
 kubectl apply -n argocd -f dev-project.yaml
+echo 'Argo CD admin pass:'
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
